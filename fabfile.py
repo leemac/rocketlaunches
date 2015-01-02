e2e88ecb75face6353ff052d17662e14f29ad9c7
Lee McKinnon <leemac314@gmail.com>
2015-01-02 13:30:04 -0800

    moved fabfile

---
+++ fabfile.py
@@ -0,0 +1,26 @@
+from fabric.api import *
+
+
+def deploy():
+    code_dir = '/srv/www/rocketlaunches.org'
+    with cd(code_dir):
+        run("git pull")    
+        run('sudo pip install -r requirements.txt')      
+        run('python manage.py syncdb --noinput')      
+
+# Start, Restart, stop
+
+def start_server():
+    run_dir = '/srv/www/rocketlaunches.org'
+    with cd(run_dir):
+        run("uwsgi --ini jobsite.ini")   
+
+def restart_server():
+    run_dir = '/srv/www/'
+    with cd(run_dir):
+        run("kill -HUP `cat /tmp/rocketlaunches.org/rocketlaunches.org`")      
+
+def stop_server():
+    run_dir = '/srv/www/'
+    with cd(run_dir):
+        run("uwsgi --stop /tmp/rocketlaunches.org/rocketlaunches.org")    
