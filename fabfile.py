5fe66b0db138a3010cdd2322d0cffb11f01231dd
Lee McKinnon <leemac314@gmail.com>
2015-01-03 13:39:47 -0800

    new fabfile command

--- fabfile.py
+++ fabfile.py
@@ -2,8 +2,8 @@ from fabric.api import *
 
 
 def deploy():
-    code_dir = '/srv/www/rocketlaunches.org'
-    with cd(code_dir):
+    run_dir = '/srv/www/rocketlaunches.org'
+    with cd(run_dir):
         run("git pull origin master")    
         run('sudo pip install -r requirements.txt')      
         run('python rocketlaunches/manage.py migrate')      
@@ -21,6 +21,13 @@ def restart_server():
         run("kill -HUP `cat /tmp/rocketlaunches.pid`")      
 
 def stop_server():
-    run_dir = '/srv/www/'
+    run_dir = '/srv/www/rocketlaunches.org/rocketlaunches'
     with cd(run_dir):
         run("uwsgi --stop /tmp/rocketlaunches.pid")    
+
+def update_data():
+    run_dir = '/srv/www/rocketlaunches.org/rocketlaunches'
+    with cd(run_dir):
+        run("python manage.py migrate --settings=rocketlaunches.settings.production")    
+        run("python manage.py loaddata rockets.json --settings=rocketlaunches.settings.production")    
+        run("python manage.py import_data --settings=rocketlaunches.settings.production")    
