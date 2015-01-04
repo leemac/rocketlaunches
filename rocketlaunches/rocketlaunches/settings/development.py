8d13e12716cc9088417889c7fb820c35b90f00fa
lee mckinnon <leemac314@gmail.com>
2015-01-04 13:30:21 -0500

    fixed desktop version css

--- rocketlaunches/rocketlaunches/settings/development.py
+++ rocketlaunches/rocketlaunches/settings/development.py
@@ -2,6 +2,11 @@ from rocketlaunches.settings.base import *
 
 DEBUG = True
 
+ALLOWED_HOSTS = [
+	'127.0.0.1',
+	'localhost'
+]
+
 DATABASES = {
    'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
