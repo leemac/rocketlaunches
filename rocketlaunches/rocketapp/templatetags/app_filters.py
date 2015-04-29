0fd5302b6434f97d5c2043820dd81e3f67501f5b
Lee McKinnon <leemac314@gmail.com>
2015-04-28 20:27:10 -0400

    fixing issue with 500 error

--- rocketlaunches/rocketapp/templatetags/app_filters.py
+++ rocketlaunches/rocketapp/templatetags/app_filters.py
@@ -8,21 +8,7 @@ register = template.Library()
 
 @register.filter(name='get_launch_displaytext')
 def get_launch_displaytext(value):
-	today = timezone.now()
-
-	if value > today:
-		return humanize.naturaltime(value)
-	elif value < today:
-		delta = today - value;
-		isRecent = delta.days == 3
-
-		if isRecent:
-			return humanize.naturaltime(value)
-		else:
-			return value.strftime('%A %B %e %y at %l:%M %p')
-
-	else:
-		return "Nothing"
+	return humanize.naturaltime(value)
 
 @register.filter(name='get_launch_displaytext_extra')
 def get_launch_displaytext_extra(launch):
