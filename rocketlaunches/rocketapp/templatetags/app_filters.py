cdfc2d533fbc3d556e3afcaefe735ac12680bdbc
Lee McKinnon <leemac314@gmail.com>
2015-04-30 08:03:43 -0400

    listing UI updates

--- rocketlaunches/rocketapp/templatetags/app_filters.py
+++ rocketlaunches/rocketapp/templatetags/app_filters.py
@@ -8,16 +8,21 @@ register = template.Library()
 
 @register.filter(name='get_launch_displaytext')
 def get_launch_displaytext(value):
-	return humanize.naturaltime(value)
-
-@register.filter(name='get_launch_displaytext_extra')
-def get_launch_displaytext_extra(launch):
 	today = timezone.now()
 
-	if launch.launch_date > today:
-		theTime = launch.launch_date.strftime('%A %B %e %y at %l:%M %p')
+	if value < today:
+		return value.strftime('%A %B %e %y at %l:%M %p')
+	else:
+		return humanize.naturaltime(value)
+
+@register.filter(name='get_launch_displaytext_extra')
+def get_launch_displaytext_extra(value):
+	today = timezone.now()
+
+	if value > today:
+		theTime = value.strftime('%A %B %e %y at %l:%M %p')
 		html = "<br/><small title='" + theTime + "'>" + theTime + " Local Time</small>"
 
 		return humanize.naturaltime(html)
 	else:
-		return ""
+		return "<br/><small>" + humanize.naturaltime(value) + "</small>"
