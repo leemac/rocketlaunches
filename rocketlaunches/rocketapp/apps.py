23e4d54cf68393d4d95f49af49196bb6093adb88
Lee McKinnon <leemac314@gmail.com>
2016-05-08 20:40:41 -0400

    migrating to django 1.9.x

---
+++ rocketlaunches/rocketapp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class RocketappConfig(AppConfig):
+    name = 'rocketapp'
--- rocketlaunches/rocketlaunches/wsgi.py
+++ rocketlaunches/rocketlaunches/wsgi.py
@@ -4,11 +4,13 @@ WSGI config for rocketlaunches project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rocketlaunches.settings")
 
 from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rocketlaunches.settings")
+
 application = get_wsgi_application()
